dac4c52eae9acf4c5c69b1c373fcc7052f2394c8
HamzaHashone <hamzadev.hashone@gmail.com>
2025-12-09 15:11:04 +0500

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,8 @@ dotenv.config();
 // Allowed origins
 const allowedOrigins = [
   "http://localhost:3000",
-  process.env.FRONTEND_URL
+  process.env.FRONTEND_URL,
+  "https://ecommerce-hijaab-collection.vercel.app"
 ].filter(Boolean);
 
 // CORS middleware
