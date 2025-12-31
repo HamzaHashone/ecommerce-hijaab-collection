44a622f7b695c3c5ff332db97599413ca754efa1
HamzaHashone <hamzadev.hashone@gmail.com>
2025-12-31 18:10:20 +0500

    add rewrites (proxy)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-host',
+            value: process.env.NEXT_PUBLIC_API_URL,
+          },
+        ],
+      },
+    ]
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
