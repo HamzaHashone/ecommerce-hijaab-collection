60ca356731620f6d69ee92d0fd532a1bb8201097
HamzaHashone <hamzadev.hashone@gmail.com>
2025-12-31 19:12:57 +0500

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
     return [
       {
         source: "/api/v2/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}:path*`,
       },
     ];
   },
